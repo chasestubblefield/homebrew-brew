1bd8cd6f49311ebed87da85c7b359dc1e0ab1667
Chase Stubblefield <chasestubblefield@gmail.com>
2017-12-29 22:56:45 -0800

    conform to brew audit

--- git-status-line.rb
+++ git-status-line.rb
@@ -1,12 +1,16 @@
-require "formula"
-
 class GitStatusLine < Formula
-  version "0.2.0"
-  url "https://github.com/chasetopher/git-status-line/releases/download/0.2.0/git-status-line-v0.2.0-x86_64-apple-darwin.tar.gz"
+  desc "Git status formatter for use with command-line prompts"
   homepage "https://github.com/chasetopher/git-status-line"
+
+  url "https://github.com/chasetopher/git-status-line/releases/download/0.2.0/git-status-line-v0.2.0-x86_64-apple-darwin.tar.gz"
+  version "0.2.0"
   sha256 "0c7893318bf8396c4268b05a2ca88b34037c0ba353b4954ff5d4602381a61105"
 
   def install
     bin.install Dir["bin/*"]
+  end
+
+  test do
+    system "false"
   end
 end
--- wit.rb
+++ wit.rb
@@ -1,12 +1,17 @@
-require "formula"
-
 class Wit < Formula
-  url "https://wit.wiimm.de/download/wit-v3.01a-r7464-mac.tar.gz"
+  desc "Tools for working with WBFS partitions"
   homepage "https://wit.wiimm.de/"
+
+  url "https://wit.wiimm.de/download/wit-v3.01a-r7464-mac.tar.gz"
+  version "3.01a-r7464"
   sha256 "e537efb4945eb9174718172a224584ce78d5f7764dcd617cbf9647a42785ae20"
 
   def install
     bin.install Dir["bin/*"]
     pkgshare.install Dir["share/*"]
+  end
+
+  test do
+    system "false"
   end
 end
