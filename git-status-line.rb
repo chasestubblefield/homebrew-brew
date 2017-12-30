605beb4db682e9a4f49da0b0fc1ab75a93254477
Chase Stubblefield <chasestubblefield@gmail.com>
2017-12-29 21:43:16 -0800

    conform to brew style

--- git-status-line.rb
+++ git-status-line.rb
@@ -1,14 +1,12 @@
-require 'formula'
+require "formula"
 
 class GitStatusLine < Formula
-
-  version '0.2.0'
-  url 'https://github.com/chasetopher/git-status-line/releases/download/0.2.0/git-status-line-v0.2.0-x86_64-apple-darwin.tar.gz'
-  homepage 'https://github.com/chasetopher/git-status-line'
-  sha256 '0c7893318bf8396c4268b05a2ca88b34037c0ba353b4954ff5d4602381a61105'
+  version "0.2.0"
+  url "https://github.com/chasetopher/git-status-line/releases/download/0.2.0/git-status-line-v0.2.0-x86_64-apple-darwin.tar.gz"
+  homepage "https://github.com/chasetopher/git-status-line"
+  sha256 "0c7893318bf8396c4268b05a2ca88b34037c0ba353b4954ff5d4602381a61105"
 
   def install
-    bin.install Dir['bin/*']
+    bin.install Dir["bin/*"]
   end
-
 end
--- wit.rb
+++ wit.rb
@@ -1,14 +1,12 @@
-require 'formula'
+require "formula"
 
 class Wit < Formula
-
-  url 'https://wit.wiimm.de/download/wit-v3.01a-r7464-mac.tar.gz'
-  homepage 'https://wit.wiimm.de/'
-  sha256 'e537efb4945eb9174718172a224584ce78d5f7764dcd617cbf9647a42785ae20'
+  url "https://wit.wiimm.de/download/wit-v3.01a-r7464-mac.tar.gz"
+  homepage "https://wit.wiimm.de/"
+  sha256 "e537efb4945eb9174718172a224584ce78d5f7764dcd617cbf9647a42785ae20"
 
   def install
-    bin.install Dir['bin/*']
-    pkgshare.install Dir['share/*']
+    bin.install Dir["bin/*"]
+    pkgshare.install Dir["share/*"]
   end
-
 end
