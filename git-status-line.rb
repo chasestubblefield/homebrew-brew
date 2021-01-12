4b5f08e59b1183317a2c5004d874b3fe93b3dbb4
Chase Stubblefield <chasestubblefield@gmail.com>
2021-01-12 12:11:25 -0800

    git-status-line 0.3.0

--- git-status-line.rb
+++ git-status-line.rb
@@ -1,10 +1,10 @@
 class GitStatusLine < Formula
   desc "Git status formatter for use with command-line prompts"
-  homepage "https://github.com/scstubblefield/git-status-line"
+  homepage "https://github.com/chasestubblefield/git-status-line"
 
-  url "https://github.com/scstubblefield/git-status-line/releases/download/0.2.0/git-status-line-v0.2.0-x86_64-apple-darwin.tar.gz"
-  version "0.2.0"
-  sha256 "0c7893318bf8396c4268b05a2ca88b34037c0ba353b4954ff5d4602381a61105"
+  url "https://github.com/chasestubblefield/git-status-line/releases/download/0.3.0/git-status-line-v0.3.0-x86_64-apple-darwin.tar.gz"
+  version "0.3.0"
+  sha256 "e8ac579dca4b44feabc0c1082283d0a978b2e8ef43a4f4dc9713a3423a080585"
 
   def install
     bin.install Dir["bin/*"]
