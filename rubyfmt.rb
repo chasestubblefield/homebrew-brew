b87ea87a73ccbe9635f474dac289d7682ebee65f
Chase Stubblefield <chasestubblefield@gmail.com>
2022-01-06 03:29:04 -0800

    Add rubyfmt

---
+++ rubyfmt.rb
@@ -0,0 +1,11 @@
+class Rubyfmt < Formula
+  desc "Ruby autoformatter"
+  homepage "https://github.com/penelopezone/rubyfmt"
+  url "https://github.com/penelopezone/rubyfmt/releases/download/v0.7.6/rubyfmt-v0.7.6-Darwin.tar.gz"
+  sha256 "839c3287e44b9584d665cf6ee780e990ff7ca1f761d63fbb55dd1d5476b4fbca"
+  license "MIT"
+
+  def install
+    bin.install "tmp/releases/v0.7.6-Darwin/rubyfmt"
+  end
+end
