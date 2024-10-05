9991e56da81c2c17269e32d3be51cc7bcc40ba87
Chase Stubblefield <chasestubblefield@gmail.com>
2024-10-04 21:31:23 -0700

    Remove redundant version numbers

--- git-status-line.rb
+++ git-status-line.rb
@@ -3,7 +3,6 @@ class GitStatusLine < Formula
   homepage "https://github.com/chasestubblefield/git-status-line"
 
   url "https://github.com/chasestubblefield/git-status-line/releases/download/0.3.0/git-status-line-v0.3.0-x86_64-apple-darwin.tar.gz"
-  version "0.3.0"
   sha256 "e8ac579dca4b44feabc0c1082283d0a978b2e8ef43a4f4dc9713a3423a080585"
 
   def install
--- gzinject.rb
+++ gzinject.rb
@@ -3,7 +3,6 @@ class Gzinject < Formula
   homepage "https://github.com/krimtonz/gzinject"
 
   url "https://github.com/krimtonz/gzinject/archive/refs/tags/0.3.1.tar.gz"
-  version "0.3.1"
   sha256 "048b2d1cc0805c1ca73a6ee5bd55e8cc6ce40e1f634cd11d771f8f4d6a832351"
 
   def install
